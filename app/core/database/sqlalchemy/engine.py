e46bd9b2d7b5dd6c08c9bcf152bf6014d3e83794
Fedorov Semen <FedorovSemen28@yandex.ru>
2025-03-17 05:09:51 +0300

    create alchemy async engine

---
+++ app/core/database/sqlalchemy/engine.py
@@ -0,0 +1,19 @@
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
+
+
+class AsyncDatabaseEngine:
+    def __init__(
+        self,
+        url: str,
+        echo: bool = False,
+        echo_pool: bool = False,
+        pool_size: int = 5,
+        max_overflow: int = 10,
+    ) -> None:
+        self.engine: AsyncEngine = create_async_engine(
+            url=url,
+            echo=echo,
+            echo_pool=echo_pool,
+            pool_size=pool_size,
+            max_overflow=max_overflow,
+        )
