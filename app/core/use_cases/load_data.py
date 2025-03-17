bec362ae5899e981d134f9cf8eec4bc6ca8626be
Fedorov Semen <FedorovSemen28@yandex.ru>
2025-03-17 05:41:16 +0300

    create class of business logic to load fixtures

---
+++ app/core/use_cases/load_data.py
@@ -0,0 +1,23 @@
+import logging
+
+from app.core.repositories.load_repository import LoadDataRepository
+
+
+class LoadDataInteractor:
+    def __init__(self, repository: LoadDataRepository) -> None:
+        self.repository = repository
+
+    async def load_data(self, data: list[dict]) -> None:
+        logging.info("Starting load the data...")
+        repository = self.repository
+        for item in data:
+            await repository.load_data(item)
+        logging.info("The data loaded successfully")
+
+    async def load_documents(self, documents: list[dict]) -> None:
+        logging.info("Starting load the documents...")
+        repository = self.repository
+        for document in documents:
+            print(document)
+            await repository.load_documents(document)
+        logging.info("The documents loaded successfully")
